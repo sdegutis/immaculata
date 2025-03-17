442efa8b9e2d2aa96396ea475dbf93dcbbbff278
sdegutis <sbdegutis@gmail.com>
2025-03-17 10:49:08 -0500

    Export default jsx impls.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { startDevServer } from './dev-server.js'
 export { generateFiles } from './file-generator.js'
 export { File } from './file.js'
+export * as jsxDom from './jsx-dom.js'
+export * as jsxStrings from './jsx-strings.js'
 export { Module } from './module.js'
 export { Runtime } from './runtime.js'
 export * from './ssp.js'
