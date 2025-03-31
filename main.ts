4d55f7f184a82ea3586f9bb0bcaccfede51c35e5
sdegutis <sbdegutis@gmail.com>
2025-03-31 07:01:13 -0500

    Testing live imports.

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 import * as immaculata from "immaculata"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
 
 const server = new immaculata.DevServer(8080, '/hmr')
-
 const tree = new immaculata.LiveTree('site', import.meta.url)
+
+// tree.enableModules(immaculata.makeSwcTransformJsx(() => 'immaculata/jsx-strings.ts'))
+tree.enableModules()
 
 processSite()
 
@@ -10,6 +15,9 @@ function processSite() {
   const start = Date.now()
   const map = tree.processFiles(files => {
     files = files.filter(f => !f.path.endsWith('x'))
+    console.log('in here1')
+    require('./site/test1.tsx')
+    console.log('in here2')
     return files
   })
   console.log(`Time: ${Date.now() - start} ms`)
