a3b314a1139f0ef78ba42a91a516d66c919ba732
sdegutis <sbdegutis@gmail.com>
2025-03-31 07:27:01 -0500

    Fixed bug in ssp.

--- ssp.ts
+++ ssp.ts
@@ -1,8 +1,8 @@
 import { createRequire } from "module"
 import type { LiveFile, LiveTree } from "./livetree.ts"
 
-export const isArrayFile = /\/.*(?<slug>\[.+\]).*\..+\.tsx?$/
-export const isSingleFile = /\..+\.tsx?$/
+export const isArrayFile = /\/.*(?<slug>\[.+\]).*\..+(?<ext>\.tsx?)$/
+export const isSingleFile = /\..+(?<ext>\.tsx?)$/
 
 export function processFile(tree: LiveTree, file: LiveFile): LiveFile[] {
   console.log(tree.base)
@@ -15,12 +15,12 @@ export function processFile(tree: LiveTree, file: LiveFile): LiveFile[] {
     const exportedArray = require('.' + file.path).default as [string, any][]
     for (const [name, content] of exportedArray) {
       const filepath = file.path.replace(match.groups!["slug"]!, name)
-      out.push({ path: filepath.slice(0, -3), content })
+      out.push({ path: filepath.slice(0, -match.groups!["ext"]!.length), content })
     }
   }
-  else if (file.path.match(isSingleFile)) {
+  else if (match = file.path.match(isSingleFile)) {
     const exportedContent = require('.' + file.path).default
-    out.push({ path: file.path.slice(0, -3), content: exportedContent })
+    out.push({ path: file.path.slice(0, -match.groups!["ext"]!.length), content: exportedContent })
   }
   else {
     out.push(file)
