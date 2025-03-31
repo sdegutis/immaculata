7bf97d142d8189dafdbdc255cbc1d695f71470cc
sdegutis <sbdegutis@gmail.com>
2025-03-31 05:58:51 -0500

    Use path.

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-import { LiveTree } from "./livetree.ts"
+import * as immaculata from "immaculata"
 
-const tree = new LiveTree('site', import.meta.url)
-tree.enableModules()
+const tree = new immaculata.LiveTree('site', import.meta.url)
+
+// const tsx = immaculata.makeSwcTransformJsx(() => 'immaculata/src/jsx-strings.ts')
+const tsx = immaculata.makeSwcTransformJsx(root => root + '/jsx-runtime.ts')
+
+tree.enableModules(tsx)
+
 tree.watch({}, async (paths) => {
   console.log('paths changed', paths)
 
--- nodemodule.d.ts
+++
@@ -1,8 +0,0 @@
-import "module"
-
-declare module "module" {
-  export function registerHooks(opts: {
-    load?: LoadHook,
-    resolve?: ResolveHook,
-  }): void
-}
