580c7000098949519ab65b279d71528a914a2b10
sdegutis <sbdegutis@gmail.com>
2025-03-31 05:17:48 -0500

    Export LiveTree.

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ import { generateFiles } from './file-generator.ts'
 import { File } from './file.ts'
 import * as jsxDomImpl from './jsx-dom.ts'
 import * as jsxStringsImpl from './jsx-strings.ts'
+import { LiveTree } from './livetree.ts'
 import { Module } from './module.ts'
 import { Runtime } from './runtime.ts'
 import { processFile, processSite } from './ssp.ts'
 
 export default {
 
-  Runtime,
   startDevServer,
   generateFiles,
+  LiveTree,
 
   deprecated: {
+    Runtime,
     jsxDomImpl,
     jsxStringsImpl,
     Module,
