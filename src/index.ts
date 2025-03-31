5d9c92225e7fd758ffd3a109846db4557e19f4dc
sdegutis <sbdegutis@gmail.com>
2025-03-31 05:27:26 -0500

    Back to this.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
-import { startDevServer } from './dev-server.ts'
-import { generateFiles } from './file-generator.ts'
-import { File } from './file.ts'
-import * as jsxDomImpl from './jsx-dom.ts'
-import * as jsxStringsImpl from './jsx-strings.ts'
-import { LiveTree } from './livetree.ts'
-import { Module } from './module.ts'
-import { Runtime } from './runtime.ts'
-import { processFile, processSite } from './ssp.ts'
+export { startDevServer } from './dev-server.ts'
+export { generateFiles } from './file-generator.ts'
+export { File } from './file.ts'
+export * as jsxDomImpl from './jsx-dom.ts'
+export * as jsxStringsImpl from './jsx-strings.ts'
+export { LiveTree, makeSwcTransformJsx, type JsxTransformer } from './livetree.ts'
+export { Module } from './module.ts'
+export { Runtime } from './runtime.ts'
+export { processFile, processSite } from './ssp.ts'
 
-export default {
-
-  startDevServer,
-  generateFiles,
-  LiveTree,
-
-  deprecated: {
-    Runtime,
-    jsxDomImpl,
-    jsxStringsImpl,
-    Module,
-    File,
-    processFile,
-    processSite,
-  }
-
-}
