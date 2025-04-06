3bd59d4ddd4077609e382f667aaff98e74ec090a
sdegutis <sbdegutis@gmail.com>
2025-04-06 15:42:36 -0500

    Use new methods.

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 import { transformSync, type Options } from '@swc/core'
 import { randomUUID } from 'crypto'
 import { readFileSync } from 'fs'
-import { LiveTree } from 'immaculata'
+import { LiveTree, tryTsTsxJsxModuleHook } from 'immaculata'
 import { registerHooks } from 'module'
-import { relative } from 'path/posix'
 import { fileURLToPath } from 'url'
 
 const tree = new LiveTree('site', import.meta.url)
@@ -14,66 +13,8 @@ tree.watch({}, () => {
   import('./site/a.js')
 })
 
-registerHooks({
-
-  resolve: (spec, context, next) => {
-    if (!spec.match(/^(\.|\/|file:\/\/\/)/)) return next(spec, context)
-
-    let path = new URL(spec, context.parentURL).href
-    if (!path.startsWith(tree.base)) return next(spec, context)
-
-    const found = tree.files.get('/' + relative(tree.base, path))
-    if (!found) return next(spec, context)
-
-    if (context.parentURL?.startsWith(tree.base) && !context.parentURL.endsWith('/noop.js')) {
-      const depending = context.parentURL.slice(tree.base.length).replace(/\?ver=\d+$/, '')
-      const depended = path.slice(tree.base.length)
-      tree.addDep(depending, depended)
-    }
-
-    const newurl = new URL('.' + found.path, tree.base + '/')
-    newurl.search = `ver=${found.version}`
-
-    return { url: newurl.href, shortCircuit: true }
-  },
-
-  load: (url, context, next) => {
-    if (url.startsWith(tree.base)) {
-      url = url.replace(/\?ver=\d+$/, '')
-
-      const path = url.slice(tree.base.length)
-      const found = tree.files.get(path)
-      if (!found) return next(url, context)
-
-      // console.log('loading from tree')
-      return {
-        shortCircuit: true,
-        format: found.path.match(/\.tsx?$/) ? 'module-typescript' : 'module',
-        source: found.content,
-      }
-    }
-    return next(url, context)
-  }
-})
-
-registerHooks({
-  resolve: (spec, ctx, next) => {
-    const trySpec = (spec: string) => {
-      try { return next(spec, ctx) }
-      catch (e: any) {
-        if (e.code !== 'ERR_MODULE_NOT_FOUND') throw e
-        return null
-      }
-    }
-    return (
-      trySpec(spec) ??
-      trySpec(spec.replace(/\.js(\?|$)/, '.ts$1')) ??
-      trySpec(spec.replace(/\.js(\?|$)/, '.tsx$1')) ??
-      trySpec(spec.replace(/\.js(\?|$)/, '.jsx$1')) ??
-      next(spec, ctx)
-    )
-  },
-})
+registerHooks(tree.moduleHook())
+registerHooks(tryTsTsxJsxModuleHook)
 
 registerHooks({
   load: (url, context, next) => {
