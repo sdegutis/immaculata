0f7d97cabe4908fc0e6e207b1c90d10d9683678a
sdegutis <sbdegutis@gmail.com>
2025-04-06 16:00:57 -0500

    Split tsx into helper.

--- main.ts
+++ main.ts
@@ -16,58 +16,73 @@ tree.watch({}, () => {
 registerHooks(tree.moduleHook())
 registerHooks(tryTsTsxJsxModuleHook)
 
-registerHooks({
-  load: (url, context, next) => {
-    const istsx = url.match(/\.tsx\??/)
-    const isjsx = url.match(/\.jsx\??/)
-    if (!isjsx && !istsx) return next(url, context)
+registerHooks(compileTsxHook((source, types) => {
 
-    let source: string
-    try { source = next(url, context).source!.toString() }
-    catch (e: any) {
-      if (e.code !== 'ERR_UNKNOWN_FILE_EXTENSION') throw e
-      source = readFileSync(fileURLToPath(url), 'utf8')
-    }
 
-    const opts: Options = {
-      isModule: true,
-      sourceMaps: 'inline',
-      jsc: {
-        keepClassNames: true,
-        target: 'esnext',
-        parser: { syntax: 'typescript', tsx: true, decorators: true },
-        transform: {
-          react: {
-            runtime: 'automatic',
-            importSource: '/jsx.js',
-          },
+  const opts: Options = {
+    isModule: true,
+    sourceMaps: 'inline',
+    jsc: {
+      keepClassNames: true,
+      target: 'esnext',
+      parser: { syntax: 'typescript', tsx: true, decorators: true },
+      transform: {
+        react: {
+          runtime: 'automatic',
+          importSource: '/jsx.js',
         },
       },
-    }
-
-    opts.jsc ??= {}
-    opts.jsc.parser = istsx
-      ? { syntax: 'typescript', tsx: true, decorators: true }
-      : { syntax: 'ecmascript', jsx: true, decorators: true }
-    opts.jsc ??= {}
-    opts.jsc.transform ??= {}
-    opts.jsc.transform.react ??= {}
-    opts.jsc.transform.react.importSource = tree.base + '/reactlike.ts'
-
-
-    let fixJsxImport
-    if (opts.jsc?.transform?.react?.importSource) {
-      const uuid = randomUUID()
-      const fakeImport = `${uuid}/jsx-runtime`
-      const realImport = opts.jsc.transform.react.importSource
-      opts.jsc.transform.react.importSource = uuid
-      fixJsxImport = (code: string) => code.replace(fakeImport, realImport)
-    }
-
-    source = transformSync(source, opts).code
-    if (fixJsxImport) source = fixJsxImport(source)
-    return { source, format: 'module', shortCircuit: true }
+    },
   }
-})
+
+  opts.jsc ??= {}
+  opts.jsc.parser = types
+    ? { syntax: 'typescript', tsx: true, decorators: true }
+    : { syntax: 'ecmascript', jsx: true, decorators: true }
+  opts.jsc ??= {}
+  opts.jsc.transform ??= {}
+  opts.jsc.transform.react ??= {}
+  opts.jsc.transform.react.importSource = tree.base + '/reactlike.ts'
+
+
+  let fixJsxImport
+  if (opts.jsc?.transform?.react?.importSource) {
+    const uuid = randomUUID()
+    const fakeImport = `${uuid}/jsx-runtime`
+    const realImport = opts.jsc.transform.react.importSource
+    opts.jsc.transform.react.importSource = uuid
+    fixJsxImport = (code: string) => code.replace(fakeImport, realImport)
+  }
+
+  source = transformSync(source, opts).code
+  if (fixJsxImport) source = fixJsxImport(source)
+
+  return source
+
+}))
 
 import('./site/a.js')
+
+
+function compileTsxHook(fn: (src: string, types: boolean) => string): Parameters<typeof registerHooks>[0] {
+  return {
+
+    load: (url, context, next) => {
+      const istsx = url.match(/\.tsx(\?|$)/)
+      const isjsx = url.match(/\.jsx(\?|$)/)
+      if (!isjsx && !istsx) return next(url, context)
+
+      let source: string
+      try { source = next(url, context).source!.toString() }
+      catch (e: any) {
+        if (e.code !== 'ERR_UNKNOWN_FILE_EXTENSION') throw e
+        source = readFileSync(fileURLToPath(url), 'utf8')
+      }
+
+      source = fn(source, !!istsx)
+
+      return { source, format: 'module', shortCircuit: true }
+    }
+
+  }
+}
