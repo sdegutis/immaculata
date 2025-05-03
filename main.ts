54d1efd3d08377aa79f68fdc41ffa8019ce32708
sdegutis <sbdegutis@gmail.com>
2025-05-03 10:25:30 -0500

    Test.

--- main.ts
+++ main.ts
@@ -2,21 +2,29 @@ import { transformSync } from '@swc/core'
 import { compileJsxTsxModuleHook, FileTree, jsxRuntimeModuleHook, tryTsTsxJsxModuleHook } from 'immaculata'
 import { registerHooks } from 'module'
 
-const tree = new FileTree('site', import.meta.url)
+const tree = new FileTree('.', import.meta.url, {
+  exclude: (path) => {
+    if (path.includes('/.git/')) return true
+    if (path.includes('/node_modules/')) return true
+
+    return false
+  }
+})
 tree.files.keys().forEach(k => console.log('key: ', k))
 
 tree.watch({}, (paths) => {
   console.log('')
   console.log('changed')
-  paths.values().forEach(f => console.log('  ', f))
+  // paths.values().forEach(f => console.log('  ', f.change, f.path))
 
-  tree.files.keys().forEach(k => console.log('key2: ', k))
-  // import('./site/a.js')
+  // tree.files.keys().forEach(k => console.log('key2: ', k))
+  import('./site/a.js')
 })
 
 registerHooks(tree.enableImportsModuleHook())
 registerHooks(tryTsTsxJsxModuleHook)
-registerHooks(jsxRuntimeModuleHook('immaculata/dist/jsx-strings.js'))
+registerHooks(jsxRuntimeModuleHook(tree.root + '/site/reactlike.ts'))
+// registerHooks(jsxRuntimeModuleHook('immaculata/dist/jsx-strings.js'))
 registerHooks(compileJsxTsxModuleHook((source, url) => {
   return transformSync(source, {
     isModule: true,
@@ -34,4 +42,4 @@ registerHooks(compileJsxTsxModuleHook((source, url) => {
   }).code
 }))
 
-// import('./site/a.js')
+import('./site/a.js')
