d1a9882fb7b77ea06ac2b7a644439dc9b0dcc507
Steven Degutis <sbdegutis@gmail.com>
2025-05-24 16:09:14 -0500

    Fix usage of generateFiles.parent

--- src/file-generator.ts
+++ src/file-generator.ts
@@ -13,21 +13,21 @@ export function generateFiles(out: Map<string, Buffer | string>, opts?: {
   const madeDirs = new Set<string>()
   const mkdirIfNeeded = (dir: string) => {
     if (madeDirs.has(dir)) return
-    if (parent.startsWith(dir)) return
     madeDirs.add(dir)
     console.log('mkdir', dir)
     if (!dry) fs.mkdirSync(dir)
   }
 
   for (const [filepath, content] of out) {
-    const newFilepath = path.posix.join(parent, outDir, filepath)
-    const parts = newFilepath.split(path.posix.sep)
+    const relfile = path.join(outDir, filepath)
+    const parts = relfile.split(path.sep)
     for (let i = 1; i < parts.length; i++) {
-      const dir = path.posix.join(...parts.slice(0, i))
+      const dir = path.join(parent, ...parts.slice(0, i))
       mkdirIfNeeded(dir)
     }
 
-    console.log('writefile', newFilepath)
-    if (!dry) fs.writeFileSync(newFilepath, content)
+    const absfile = path.join(parent, relfile)
+    console.log('writefile', absfile)
+    if (!dry) fs.writeFileSync(absfile, content)
   }
 }
