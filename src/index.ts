ecacf7ceefd26306e1c82a7b49549f94f1d97b65
Steven Degutis <sbdegutis@gmail.com>
2025-05-31 04:51:25 -0500

    Use TypeScript to transform imports.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * as babel from './babel.js'
 export * from './dev-server.js'
 export * from './file-generator.js'
 export * from './filetree.js'
 export * as hooks from './hooks.js'
 export * from './pipeline.js'
+export * from './transforms.js'
 
---
+++ src/transforms.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from "node:fs"
+import { join } from 'node:path'
+import ts from 'typescript'
+
+export function transformImports(projectRoot: string, replacements?: Record<string, string>): ts.TransformerFactory<ts.SourceFile> {
+  return ctx => node => {
+    return ts.visitNode(node, visitor) as ts.SourceFile
+    function visitor(node: ts.Node) {
+      if (ts.isStringLiteral(node) && node.parent && (
+        ts.isImportDeclaration(node.parent) ||
+        ts.isExportDeclaration(node.parent) ||
+        (ts.isCallExpression(node.parent) && node.parent.expression.getText() === 'import')
+      )) {
+        return ts.factory.createStringLiteral(modifyPath(projectRoot, node.text, replacements))
+      }
+      return ts.visitEachChild(node, visitor, ctx)
+    }
+  }
+}
+
+function modifyPath(projectRoot: string, dep: string, replacements?: Record<string, string>): string {
+  if (dep.match(/^[./]/) || dep.startsWith('http')) return dep
+
+  if (replacements && dep in replacements) {
+    return replacements[dep]!
+  }
+
+  let split = dep.indexOf('/')
+  if (split === -1) split = dep.length
+
+  const lib = dep.slice(0, split)
+  const imported = dep.slice(split)
+
+  if (replacements && lib in replacements) {
+    return replacements[lib]! + imported
+  }
+
+  const fullpath = join(projectRoot, 'node_modules', lib, 'package.json')
+  const pkgjson = JSON.parse(readFileSync(fullpath, 'utf8'))
+  const baseurl = new URL(imported, pkgjson.homepage)
+
+  return baseurl.href
+}
