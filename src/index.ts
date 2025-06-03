b85e008397a24c1ee11f44c2ac732c0e643a6603
Steven Degutis <sbdegutis@gmail.com>
2025-06-03 11:48:45 -0500

    Remove pointless index.

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './dev-server.js'
-export * from './file-generator.js'
-export * from './filetree.js'
-export * as hooks from './hooks.js'
-export * from './pipeline.js'
-export * from './transforms.js'
-
